93d012a3eb4e6d6c95f81dde92f815da434cb696
PranoySarker <pranoysarker01@gmail.com>
2024-12-28 17:26:22 +0600

    add some functionalities to manage the cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,15 +1,39 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+
+
 class Cart:
  
     def __init__(self, request):
+    #  Initialize the cart.
+    
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
         # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def __iter__(self):
+    #  Iterate over the items in the cart.
+        product_ids = self.cart.keys()
+        # get the product objects and add them to cart
+        products  = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+    
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    
+    def __len__(self):
+        #count all items in the cart
+        return sum(item['quantity'] for item in self.cart.values())
+
 
 # Add a product to the cart or update its quantity.
     def add(self, product, quantity=1, override_quantity=False):
@@ -37,3 +61,12 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart)
